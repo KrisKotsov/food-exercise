4445c19718fb5c183cdcb8c47c764306783cb541
kkotsov@abv.bg <you@example.com>
2022-12-10 18:07:34 +0200

    exercise changes

--- scripts/fetchData.js
+++ scripts/fetchData.js
@@ -1,5 +1,7 @@
 const apiBaseUrl = "https://www.themealdb.com/api/json/v1/1";
 const categoriesEndpoint = "/categories.php";
+//https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood
+const filterEndpoint = "/filter.php";
 
 async function fetchData(url) {
     try {
@@ -11,5 +13,18 @@ async function fetchData(url) {
         console.log(err);
     }
 }
-export { apiBaseUrl, categoriesEndpoint };
+
+async function fetchMealsByCategory(category) {
+    try {
+        const url = apiBaseUrl + filterEndpoint + "?c=" + category;
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
+export { apiBaseUrl, categoriesEndpoint, fetchMealsByCategory };
 export default fetchData;
--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,54 @@
-import fetchData, { apiBaseUrl, categoriesEndpoint } from "./fetchData.js";
-import { readFromStorage, storageKeys, writeToStorage, } from "./storageControl.js";
+import fetchData, { apiBaseUrl, categoriesEndpoint, fetchMealsByCategory } from "./fetchData.js";
+import { readFromStorage, storageKeys, writeToStorage } from "./storageControl.js";
 const categoriesFilterDiv = document.getElementById("detailed-categories-filter");
+const resultsContainer = document.getElementById("results-contaienr")
+
+
+async function createMealPreviewElement(meal) {
+    const { idMeal, strMealThumb, strMeal } = meal;
+
+    const recipeDiv = document.createElement("div");
+    recipeDiv.className = "category-box";
+    recipeDiv.setAttribute("id", idMeal);
+
+    const recipeImg = document.createElement("img");
+    recipeImg.setAttribute("src", strMealThumb);
+
+    const recipeTitle = document.createElement("h4");
+    recipeTitle.textContent = strMeal;
+    recipeDiv.appendChild(recipeImg);
+    recipeDiv.appendChild(recipeTitle);
+
+    resultsContainer.appendChild(recipeDiv);
+};
+
+
+
+
+async function showMealsByCategory(category) {
+    const { meals } = await fetchMealsByCategory(category);
+
+    resultsContainer.innerHTML = "";
+
+    meals.forEach((recipe) => {
+        createMealPreviewElement(recipe);
+    });
+
+    if (window.innerHeight < window.innerWidth) {
+        window.scrollTo(top);
+    }
+    else {
+        window.scrollTo(0, resultsContainer.offsetTop);
+    }
+}
+
 
 function createCategoryElement(categoryObj) {
     const { strCategory, strCategoryThumb } = categoryObj;
 
     const categoryDiv = document.createElement("div");
     categoryDiv.className = "category-box";
+    categoryDiv.addEventListener("click", () => showMealsByCategory(strCategory));
 
     const categoryThumb = document.createElement("img");
     categoryThumb.setAttribute("src", strCategoryThumb);
@@ -27,7 +69,7 @@ async function main() {
     if (!categories) {
         const { categories: fetchedCategories } = await fetchData(apiBaseUrl + categoriesEndpoint);
         categories = fetchedCategories;
-        writeToStorage();
+        writeToStorage(storageKeys.categories, categories);
     }
 
     categories.forEach(el => {
